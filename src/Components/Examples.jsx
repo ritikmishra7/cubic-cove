63c643bdfe77dd0e28f58efff6cab048ed3962d3
Ritik Mishra <ritikmishra7@gmail.com>
2023-09-14 03:25:56 +0530

    scrollbar added

--- src/Components/Examples.jsx
+++ src/Components/Examples.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import image1 from '../assets/images/fadeImg-1.png'
 import image2 from '../assets/images/fadeImg-2.png'
 import image3 from '../assets/images/fadeImg-3.png'
+import { Scrollbars } from 'react-custom-scrollbars-2';
 function Examples() {
     return (
         <div className='flex flex-col justify-center items-center gap-32'>
@@ -9,16 +10,14 @@ function Examples() {
                 <h2 className='font-bold'>The best sites are</h2>
                 <h2 className='text-[#F69A6B] font-bold'>#HostedOnCubicCove</h2>
             </div>
-            <div className='flex max-w-6xl gap-2 overflow-scroll mx-auto'>
-                <div>
-                    <img src={image1} alt="image1" className='w-full' />
-                </div>
-                <div>
-                    <img src={image2} alt="image2" />
-                </div>
-                <div>
-                    <img src={image3} alt="image3" />
-                </div>
+            <div className='max-w-7xl'>
+                <Scrollbars style={{ width: 1080, height: 500 }}>
+                    <div className='flex gap-4'>
+                        <img src={image1} alt="ex-1" className='hover:scale-110' style={{ transition: 'all 0.3s ease-out' }} />
+                        <img src={image2} alt="ex-2" className='hover:scale-110' style={{ transition: 'all 0.3s ease-out' }} />
+                        <img src={image3} alt="ex-3" className='hover:scale-110' style={{ transition: 'all 0.3s ease-out' }} />
+                    </div>
+                </Scrollbars>
             </div>
         </div>
 
